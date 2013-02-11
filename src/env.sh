ade1fa7cf484109b40745fb2d197f65d61d3155b
Xose Pérez <xose.perez@theetailers.com>
2013-02-11 23:37:50 +0100

    RC version of the software

--- src/env.sh
+++ src/env.sh
@@ -1,3 +1,4 @@
 export ARDUINODIR=~/local/arduino/
 export BOARD=fio
 export SERIALDEV=/dev/ttyUSB0
+export BAUDRATE=57600
--- src/weatherstation.ino
+++ src/weatherstation.ino
@@ -41,21 +41,25 @@
 // Configuration
 // ===========================================
 
+#define BAUD_RATE 57600
+
+#define USE_NOAA
+
 #define BATT_PIN 0
 #define PANEL_PIN 1
 #define XBEE_SLEEP_PIN 4
 #define DHT_PIN 12
 
 #define DHT_TYPE DHT22
-#define VOLTAGE_REFERENCE 1100
-#define BATT_VOLTAGE_FACTOR 4.06
-#define PANEL_VOLTAGE_FACTOR 4.06
-#define XBEE_DELAY 20
+#define VOLTAGE_REFERENCE 3300
+#define BATT_VOLTAGE_FACTOR 4.24 // 100KOhm + 324KOhm
+#define PANEL_VOLTAGE_FACTOR 6.61 // 100kOhm + 561kOhm
+#define XBEE_DELAY 40
 
 #define SLEEP_INTERVAL SLEEP_2S
-#define WARMUP_DELAY 2000
-#define MEASURE_EVERY 1
+#define MEASURE_EVERY 28 // each measurement takes roughly 4 seconds
 #define SEND_EVERY 5
+#define WARMUP_DELAY 2000
 
 struct measure {
     int count;
@@ -73,33 +77,63 @@ Adafruit_BMP085 bmp;
 
 boolean bmp_ready = false;
 unsigned long interval = 0;
+unsigned long measures = 0;
 
-struct measure dht22_temperature = {0,0,0,0};
-struct measure dht22_humidity = {0,0,0,0};
-struct measure bmp085_temperature = {0,0,0,0};
-struct measure bmp085_pressure = {0,0,0,0};
-struct measure fio_temperature = {0,0,0,0};
-struct measure fio_voltage = {0,0,0,0};
+struct measure dht22_temperatures = {0,0,0,0};
+struct measure dht22_humidities = {0,0,0,0};
+struct measure bmp085_temperatures = {0,0,0,0};
+struct measure bmp085_pressures = {0,0,0,0};
+struct measure fio_temperatures = {0,0,0,0};
+struct measure fio_voltages = {0,0,0,0};
+struct measure battery_voltages = {0,0,0,0};
+struct measure panel_voltages = {0,0,0,0};
 
 // ===========================================
 // Methods
 // ===========================================
 
+/*
+ * xbeeSleep
+ * Sets the XBee radio to sleep
+ *
+ * @return void
+ */
 void xbeeSleep() {
     delay(XBEE_DELAY);
-    digitalWrite(XBEE_SLEEP_PIN, HIGH);
+    pinMode(XBEE_SLEEP_PIN, INPUT_PULLUP);
 }
 
+/*
+ * xbeeWake
+ * Wakes up the XBee radio to sleep
+ *
+ * @return void
+ */
 void xbeeWake() {
+    pinMode(XBEE_SLEEP_PIN, OUTPUT);
     digitalWrite(XBEE_SLEEP_PIN, LOW);
     delay(XBEE_DELAY);
 }
 
+/*
+ * readVoltage
+ * Reads the analog PIN and performs the conversion to get mV
+ *
+ * @param byte pin PIN number
+ * @param float factor conversion factor based on voltage divider
+ * @return long
+ */
 long readVoltage(byte pin, float factor) {
     int reading = analogRead(pin);
     return (long) map(reading, 0, 1023, 0, VOLTAGE_REFERENCE) * factor;
 }
 
+/*
+ * readVoltage
+ * Reads the internal 328 voltage register
+ *
+ * @return long
+ */
 long readVoltage() {
     long result;
     // Read 1.1V reference against AVcc
@@ -113,6 +147,12 @@ long readVoltage() {
     return result;
 }
 
+/*
+ * readTemperature
+ * Reads the internal 328 temperature register
+ *
+ * @return float
+ */
 float readTemperature() {
     signed long resultTemp;
     // Read temperature sensor against 1.1V reference
@@ -125,10 +165,20 @@ float readTemperature() {
     return (float) resultTemp;
 }
 
-// dewPoint function NOAA
-// reference: http://wahiduddin.net/calc/density_algorithms.htm 
-float dewPoint(float celsius, float humidity) {
-    float A0= 373.15/(273.15 + celsius);
+#ifdef USE_NOAA
+
+/*
+ * dewPoint
+ * Calculates dew point
+ *
+ * reference: http://wahiduddin.net/calc/density_algorithms.htm 
+ *
+ * @param temperature float temperature in celsius
+ * @param humidity float humidity in %
+ * @return float
+ */
+float dewPoint(float temperature, float humidity) {
+    float A0= 373.15/(273.15 + temperature);
     float SUM = -7.90298 * (A0-1);
     SUM += 5.02808 * log10(A0);
     SUM += -1.3816e-7 * (pow(10, (11.344*(1-1/A0)))-1) ;
@@ -139,24 +189,49 @@ float dewPoint(float celsius, float humidity) {
     return (241.88 * T) / (17.558 - T);
 }
 
-// delta max = 0.6544 wrt dewPoint()
-// 5x faster than dewPoint()
-// reference: http://en.wikipedia.org/wiki/Dew_point
+#else
+
 /*
-float dewPoint(float celsius, float humidity) {
+ * dewPoint
+ * Calculates dew point
+ * delta max = 0.6544 wrt dewPoint()
+ * 5x faster than dewPoint()
+ * reference: http://en.wikipedia.org/wiki/Dew_point
+ *
+ * @param temperature float temperature in celsius
+ * @param humidity float humidity in %
+ * @return float
+ */
+float dewPoint(float temperature, float humidity) {
     float a = 17.271;
     float b = 237.7;
-    float temp = (a * celsius) / (b + celsius) + log(humidity/100);
+    float temp = (a * temperature) / (b + temperature) + log(humidity/100);
     float Td = (b * temp) / (a - temp);
     return Td;
 }
-*/
 
+#endif
+
+/*
+ * reset
+ * Resets the structure
+ *
+ * @param measure magnitude
+ * @return void
+ */
 void reset(measure &magnitude) {
     magnitude.count = 0;
     magnitude.sum = 0;
 }
 
+/*
+ * record
+ * Records the value into a measure structure
+ *
+ * @param measure magnitude
+ * @param float value
+ * @return void
+ */
 void record(measure &magnitude, float value) {
     magnitude.sum = magnitude.sum + value;
     magnitude.count = magnitude.count + 1;
@@ -169,7 +244,14 @@ void record(measure &magnitude, float value) {
     }
 }
 
-float get_average(measure magnitude) {
+/*
+ * calculateAverage
+ * Calculates the average for the stored values substracting the minimum and maximum first
+ *
+ * @param measure magnitude
+ * @return float
+ */
+float calculateAverage(measure magnitude) {
     if (magnitude.count > 2) {
         return (magnitude.sum - magnitude.minimum - magnitude.maximum) / (magnitude.count - 2);
     } else {
@@ -177,102 +259,140 @@ float get_average(measure magnitude) {
     }
 }
 
+/*
+ * readAll
+ * Reads all magnitudes and stores them in metric structures
+ *
+ * @return void
+ */
 void readAll() {
 
+    // Allowing the DHT22 to warm up
     delay(WARMUP_DELAY);
 
     DHT22_ERROR_t errorCode = dht.readData();
     if (errorCode == DHT_ERROR_NONE) {
-        record(dht22_temperature, dht.getTemperatureC());
-        record(dht22_humidity, dht.getHumidity());
+        record(dht22_temperatures, dht.getTemperatureC());
+        record(dht22_humidities, dht.getHumidity());
     }
 
     if (bmp_ready) {
-        record(bmp085_temperature, bmp.readTemperature());
-        record(bmp085_pressure, bmp.readPressure());
+        record(bmp085_temperatures, bmp.readTemperature());
+        record(bmp085_pressures, bmp.readPressure());
     }
-    record(fio_temperature, readTemperature());
-    record(fio_voltage, (float) readVoltage());
-
-    //long vb = readVoltage(BATT_PIN, BATT_VOLTAGE_FACTOR);
-    //long vp = readVoltage(PANEL_PIN, PANEL_VOLTAGE_FACTOR);
+    record(fio_temperatures, readTemperature());
+    record(fio_voltages, (float) readVoltage());
+    record(battery_voltages, (float) readVoltage(BATT_PIN, BATT_VOLTAGE_FACTOR));
+    record(panel_voltages, (float) readVoltage(PANEL_PIN, PANEL_VOLTAGE_FACTOR));
 
 }
 
+/*
+ * resetAll
+ * Resets all averages
+ *
+ * @return void
+ */
+void resetAll() {
+    reset(dht22_humidities);
+    reset(dht22_temperatures);
+    reset(bmp085_temperatures);
+    reset(bmp085_pressures);
+    reset(fio_temperatures);
+    reset(fio_voltages);
+    reset(battery_voltages);
+    reset(panel_voltages);
+}
+
+/*
+ * sendAll
+ * Gets averages and sends all data through UART link
+ *
+ * @return void
+ */
 void sendAll() {
+
+    float dht22_temperature = calculateAverage(dht22_temperatures);
+    float dht22_humidity = calculateAverage(dht22_humidities);
+    float dht22_dew_point = dewPoint(dht22_temperature, dht22_humidity);
+    float bmp085_temperature = calculateAverage(bmp085_temperatures);
+    float bmp085_pressure = calculateAverage(bmp085_pressures);
+    float fio_temperature = calculateAverage(fio_temperatures);
+    float fio_voltage = calculateAverage(fio_voltages);
+    float battery_voltage = calculateAverage(battery_voltages);
+    float panel_voltage = calculateAverage(panel_voltages);
 
     xbeeWake();
 
-    Serial.println("");
+    Serial.print(F("T1:"));
+    Serial.println(dht22_temperature);
 
-    Serial.print(F("dht22_temperature:"));
-    float temperature = get_average(dht22_temperature);
-    Serial.println(temperature);
-    reset(dht22_temperature);
-    delay(20);
+    Serial.print(F("H1:"));
+    Serial.println(dht22_humidity);
 
-    Serial.print(F("dht22_humidity:"));
-    float humidity = get_average(dht22_humidity);
-    Serial.println(humidity);
-    reset(dht22_humidity);
-    delay(20);
+    Serial.print(F("D1:"));
+    Serial.println(dht22_dew_point);
 
-    Serial.print(F("dht22_dew_point:"));
-    Serial.println(dewPoint(temperature, humidity));
-    delay(20);
+    Serial.print(F("T2:"));
+    Serial.println(bmp085_temperature);
 
-    Serial.print(F("bmp085_temperature:"));
-    Serial.println(get_average(bmp085_temperature));
-    reset(bmp085_temperature);
-    delay(20);
+    Serial.print(F("P2:"));
+    Serial.println(bmp085_pressure);
 
-    Serial.print(F("bmp085_pressure:"));
-    Serial.println(get_average(bmp085_pressure));
-    reset(bmp085_pressure);
-    delay(20);
+    Serial.print(F("T3:"));
+    Serial.println(fio_temperature);
 
-    Serial.print(F("fio_temperature:"));
-    Serial.println(get_average(fio_temperature));
-    reset(fio_temperature);
-    delay(20);
+    Serial.print(F("V3:"));
+    Serial.println(fio_voltage);
 
-    Serial.print(F("fio_voltage:"));
-    Serial.println(get_average(fio_voltage));
-    reset(fio_voltage);
-    delay(20);
+    Serial.print(F("V4:"));
+    Serial.println(battery_voltage);
 
-    Serial.println("");
-    delay(20);
+    Serial.print(F("V5:"));
+    Serial.println(panel_voltage);
 
     xbeeSleep();
 
 }
 
+/*
+ * setup
+ *
+ * @return void
+ */
 void setup() {
 
-    // Configure control PIN for XBee
-    pinMode(XBEE_SLEEP_PIN, OUTPUT);
-
     // Using the ADC against internal 1V1 reference for battery monitoring
-    analogReference(INTERNAL);
+    analogReference(DEFAULT);
 
     // Initialize UART
-    Serial.begin(9600);
+    Serial.begin(BAUD_RATE);
 
     // Initialize BMP085
     bmp_ready = (bool) bmp.begin();
 
+    // Make the radio LED blink
+    xbeeWake();
+    xbeeSleep();
+
 }
 
+/*
+ * loop
+ *
+ * @return void
+ */
 void loop() {
     ++interval;
-    Serial.print(".");
     if (interval % MEASURE_EVERY == 0) {
         readAll();
-    }
-    if (interval % SEND_EVERY == 0) {
-        sendAll();
         interval = 0;
+        ++measures;
+        if (measures % SEND_EVERY == 0) {
+            sendAll();
+            resetAll();
+            measures = 0;
+        }
     }
     LowPower.powerDown(SLEEP_INTERVAL, ADC_OFF, BOD_OFF);
 }
