1c9e2b016db7fe067b50b2f5d16de3bf89b78916
Xose Pérez <xose.perez@gmail.com>
2021-04-14 17:46:11 +0200

    Small make-up fixes

--- code/src/code.ino
+++ code/src/code.ino
@@ -104,12 +104,12 @@ void bmeSetup() {
     bool status = bme.begin(BME280_ADDRESS);  
     #ifdef DEBUG
     if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        Serial.print("[SENSOR] Could not find a valid BME280 sensor, check wiring, address, sensor ID!\n");
+        Serial.print("[SENSOR] SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("[SENSOR]    ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("[SENSOR]    ID of 0x56-0x58 represents a BMP 280\n");
+        Serial.print("[SENSOR]    ID of 0x60 represents a BME 280.\n");
+        Serial.print("[SENSOR]    ID of 0x61 represents a BME 680.\n");
         while (1) delay(10);
     }
     #endif
